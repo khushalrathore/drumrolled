ff65fc529430dc1d409ee2bc6d8f568194e247ba
Khushal Rathore <118611135+khushalrathore@users.noreply.github.com>
2024-04-11 00:36:28 +0530

    pianoed

--- assets/index.js
+++ assets/index.js
@@ -1,7 +1,70 @@
 document.addEventListener('keydown', function(event) {
   if (event.key === '1') {
     event.preventDefault();
+    var audio = document.getElementById('piano-1');
+    audio.pause(); 
+    audio.currentTime = 0;
+    audio.play();
+  }
+  if (event.key === '2') {
+    event.preventDefault();
+    var audio = document.getElementById('piano-2');
+    audio.pause(); 
+    audio.currentTime = 0;
+    audio.play();
+  }
+  if (event.key === '3') {
+    event.preventDefault();
     var audio = document.getElementById('piano-3');
+    audio.pause(); 
+    audio.currentTime = 0;
+    audio.play();
+  }
+  if (event.key === '4') {
+    event.preventDefault();
+    var audio = document.getElementById('piano-4');
+    audio.pause(); 
+    audio.currentTime = 0;
+    audio.play();
+  }
+  if (event.key === '5') {
+    event.preventDefault();
+    var audio = document.getElementById('piano-5');
+    audio.pause(); 
+    audio.currentTime = 0;
+    audio.play();
+  }
+  if (event.key === '6') {
+    event.preventDefault();
+    var audio = document.getElementById('piano-6');
+    audio.pause(); 
+    audio.currentTime = 0;
+    audio.play();
+  }
+  if (event.key === '7') {
+    event.preventDefault();
+    var audio = document.getElementById('piano-7');
+    audio.pause(); 
+    audio.currentTime = 0;
+    audio.play();
+  }
+  if (event.key === '8') {
+    event.preventDefault();
+    var audio = document.getElementById('piano-8');
+    audio.pause(); 
+    audio.currentTime = 0;
+    audio.play();
+  }
+  if (event.key === '9') {
+    event.preventDefault();
+    var audio = document.getElementById('piano-9');
+    audio.pause(); 
+    audio.currentTime = 0;
+    audio.play();
+  }
+  if (event.key === '0') {
+    event.preventDefault();
+    var audio = document.getElementById('piano-0');
     audio.pause(); 
     audio.currentTime = 0;
     audio.play();
